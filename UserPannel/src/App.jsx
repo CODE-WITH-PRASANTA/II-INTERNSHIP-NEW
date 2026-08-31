61e1df4d31ac757ab49ba2b561fd645cc662eac4
Santanu <santanuranjanbal@gmail.com>
2026-08-31 11:33:05 +0530

    pull

---
+++ UserPannel/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  
+} from "react-router-dom";
+
+// Layout
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+       
+
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
