daf6b4239234f2e1330e689587d03a66187ff86d
adyasa974 <adbarik60@gmail.com>
2026-09-08 16:16:36 +0530

    pull

--- Adminpannel/src/App.jsx
+++ Adminpannel/src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ReviewsModeration from "./Component/ReviewsModeration/ReviewsModeration";
+import NoticeBoard from "./Component/NoticeBoard/NoticeBoard";
+import DonationLogs from "./Component/DonationLogs/DonationLogs";
+import SupportTickets from "./Component/SupportTickets/SupportTickets";
+import Settings from "./Component/Settings/Settings";
 
 // Layout
 
@@ -8,9 +13,11 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-
-       
-       
+<Route path="/reviews" element={<ReviewsModeration/>}/>
+    <Route path="/noticeboard" element={<NoticeBoard/>}/>   
+       <Route path="/donationlogs" element={<DonationLogs/>}/>   
+       <Route path="/support" element={<SupportTickets/>}/>   
+       <Route path="/settings" element={<Settings/>}/>
 
       </Routes>
     </BrowserRouter>
