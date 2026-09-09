06450e669cd6a35ae2211ed7b76b781d9fa2a3c0
adyasa974 <adbarik60@gmail.com>
2026-09-09 12:50:22 +0530

    pull

--- Adminpannel/src/App.jsx
+++ Adminpannel/src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// Components
 import UserAccounts from "./Component/UserAccounts/UserAccounts";
 import Registrations from "./Component/Registrations/Registrations";
 import Internships from "./Component/Internships/Internships";
@@ -13,32 +15,34 @@ import PartnersOnboard from "./Component/PartnersOnboard/PartnersOnboard";
 import BlogsBoard from "./Component/BlogsBoard/BlogsBoard";
 import MediaDesk from "./Component/MediaDesk/MediaDesk";
 
+// Layout & Pages
 import MainLayout from "./Layout/Mainlayout/Mainlayout";
 import Dashboard from "./Pages/Dashboard/Dashboard";
-// Layout
- 
 
 const App = () => {
   return (
     <BrowserRouter>
-
       <Routes>
+        {/* Parent route wrapping all pages that require Sidebar & Topbar */}
         <Route path="/" element={<MainLayout />}>
+          {/* Index route: redirects root "/" to "/dashboard" */}
+          <Route index element={<Navigate to="/dashboard" replace />} />
+
+          {/* Nested routes rendered inside MainLayout's <Outlet /> */}
           <Route path="dashboard" element={<Dashboard />} />
+          <Route path="user-accounts" element={<UserAccounts />} />
+          <Route path="registrations" element={<Registrations />} />
+          <Route path="internships" element={<Internships />} />
+          <Route path="reviews-moderation" element={<ReviewsModeration />} />
+          <Route path="notice-board" element={<NoticeBoard />} />
+          <Route path="donation-logs" element={<DonationLogs />} />
+          <Route path="support-tickets" element={<SupportTickets />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="recruitment-openings" element={<RecruitmentOpenings />} />
+          <Route path="partners-onboard" element={<PartnersOnboard />} />
+          <Route path="blogs-board" element={<BlogsBoard />} />
+          <Route path="media-desk" element={<MediaDesk />} />
         </Route>
-        <Route path="/user-accounts" element={<UserAccounts />} />
-        <Route path="/registrations" element={<Registrations />} />
-        <Route path="/internships" element={<Internships />} />
-        
-       <Route path="/reviews" element={<ReviewsModeration/>}/>
-       <Route path="/noticeboard" element={<NoticeBoard/>}/>   
-       <Route path="/donationlogs" element={<DonationLogs/>}/>   
-       <Route path="/support" element={<SupportTickets/>}/>   
-       <Route path="/settings" element={<Settings/>}/>
-       <Route path="/recruitment" element={<RecruitmentOpenings/>}/>
-       <Route path="/partners" element={<PartnersOnboard/>}/>
-       <Route path="/blogsboard" element={<BlogsBoard/>}/>   
-       <Route path="/mediadesk" element={<MediaDesk/>}/>   
       </Routes>
     </BrowserRouter>
   );
